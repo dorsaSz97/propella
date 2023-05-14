fab61987ffd22d700c8ac9fbd4ce88046107d4fe
UserOfTheSite <dorsa_dorsa76@yahoo.com>
2023-05-14 20:00:46 +0200

    search box #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 // apply the styles to every route in our app
 import './globals.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 import { Poppins } from 'next/font/google';
+
+import { RxPerson } from 'react-icons/rx';
+import { RiSearchLine } from 'react-icons/ri';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { BsCalendar2Date } from 'react-icons/bs';
 
 const poppins = Poppins({
   weight: ['400', '600', '700'], // regular, bold, semibold
@@ -28,14 +35,44 @@ export default function RootLayout({
             <a href="/">Propella</a>
           </h1>
           <div className="flex gap-4 items-center">
-            <button className="border-grassGreen border-2 rounded-full px-6 py-1 text-body-lg">
+            <button className="border-grassGreen border-[1px] rounded-full px-6 py-1 text-body-lg">
               Login
             </button>
-            <button className="border-grassGreen border-2 rounded-full px-6 py-1 text-body-lg bg-grassGreen text-white">
+            <button className="border-grassGreen border-[1px] rounded-full px-6 py-1 text-body-lg bg-grassGreen text-white">
               Register
             </button>
           </div>
         </header>
+
+        {/* searchbox */}
+
+        <div className="flex justify-center items-center mt-[-2rem]">
+          <div className="flex">
+            <div className="rounded-full h-35 w-35 bg-silverGrey p-2 btn--search">
+              <button className="rounded-full h-25 w-25  p-5 text-lg">
+                <HiOutlineLocationMarker />
+              </button>
+            </div>
+
+            <div className="rounded-full h-35 w-35 bg-silverGrey p-2 btn--search">
+              <button className="rounded-full h-25 w-25  p-5 text-lg">
+                <BsCalendar2Date />
+              </button>
+            </div>
+
+            <div className="rounded-full h-35 w-35 bg-silverGrey p-2 btn--search">
+              <button className="rounded-full h-25 w-25  p-5 text-lg">
+                <RxPerson />
+              </button>
+            </div>
+
+            <div className="rounded-full h-35 w-35 bg-silverGrey p-2">
+              <button className="rounded-full h-25 w-25 bg-grassGreen p-5 text-lg">
+                <RiSearchLine color="white" />
+              </button>
+            </div>
+          </div>
+        </div>
         {children}
       </body>
     </html>
