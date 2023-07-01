4b284dae742383007bee5838217caf22110fed25
UserOfTheSite <dorsa_dorsa76@yahoo.com>
2023-07-01 22:25:58 +0200

    change to return instead of throw

--- app/(user-navigation)/favorites/page.tsx
+++ app/(user-navigation)/favorites/page.tsx
@@ -2,11 +2,13 @@ import { getCurrentUser, getFavorites } from '@/app/libs';
 import PropertiesList from '@/app/components/PropertiesList';
 import EmptyList from '@/app/components/EmptyList';
 import PageHeading from '@/app/components/PageHeading';
+import { NextResponse } from 'next/server';
 
 export default async function FavoritesPage() {
   const currentUser = await getCurrentUser();
+
   // if (!currentUser) return; // goes to the error page
-  if (!currentUser) throw new Error('No user found'); // goes to the error page
+  if (!currentUser) return new Error('No user found'); // goes to the error page
 
   const favorites = await getFavorites(currentUser);
   // if (!favorites) throw new Error("Error getting favorites of the user");
